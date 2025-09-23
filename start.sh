8d18ace5727ae9d06cd66c6a743c0c8ef6cccda3
Abdul Ahad <adibutt457@gmail.com>
2025-09-23 05:38:32 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,4 @@
-echo "🛠 Starting deployment setup..."
-
-set -e
-
-echo "📦 Updating pip..."
-pip install --upgrade pip
-
-
-echo "📥 Installing backend dependencies..."
-pip install -r backend/requirements.txt
-
-
+#!/bin/bash
 cd backend
-
-
-echo "🚀 Starting Flask backend..."
-exec gunicorn app:app --bind 0.0.0.0:$PORT
+pip install -r requirements.txt
+python -m gunicorn app:app --bind 0.0.0.0:$PORT
